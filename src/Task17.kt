96785d9e245bcd90cec75d268356130cdef637c4
Ivanna Stetsyuk <istetsyuk@aplana.com>
2019-08-14 16:41:34 +0300

    Initial commit

---
+++ src/Task17.kt
@@ -0,0 +1,20 @@
+//Задание 17: Авиакомпания
+//Поможем авиакомпании в создании программы для учета авиапарка.
+//Создайте класс Aircraft и реализуйте в нем свойства, содержащие следующее: максимальная дальность полета, вместимость бака, расход топлива на 100 км.
+//Переопределите геттер свойства, содержащего расход топлива, чтобы он самостоятельно рассчитывался из свойств дальности полета и вместимости бака.
+
+fun main(args: Array<String>) {
+    var aircraft = Aircraft()
+    aircraft.maximumRange = 201.2 //в км
+    aircraft.tankCapacity = 100.5 //в л
+    println("Расход топлива - ${aircraft.fuelConsumption} л/100км")
+}
+
+class Aircraft() {
+    var maximumRange: Double = 0.0
+    var tankCapacity: Double = 0.0
+
+    val fuelConsumption
+        get() = tankCapacity / maximumRange * 100
+}
+
