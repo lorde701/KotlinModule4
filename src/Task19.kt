65058455b60f2c662ae740586a1c090b37940ca3
Ivanna Stetsyuk <istetsyuk@aplana.com>
2019-08-15 16:41:44 +0300

    task19

---
+++ src/Task19.kt
@@ -0,0 +1,18 @@
+//Задание 19: Авиакомпания 3. Наследование
+//Создадим наследника класса Aircraft с названием Boeing747 и добавим ему свойство, обозначающее вместимость пассажиров
+
+fun main(args: Array<String>) {
+    val boeing1 = Boeing747(10.0, 12.0, 14)
+    val boeing2: Aircraft3
+    boeing2 = Boeing747(100.0, 100.0, 200)
+    println("Расход топлива boeing1- ${boeing1.fuelConsumption}, вместимость пассажиров boeing1 - ${boeing1.passengerCapacity}")
+    println("Расход топлива boeing2 - ${boeing2.fuelConsumption}, вместимость пассажиров boeing2 - ${boeing2.passengerCapacity}")
+}
+
+class Boeing747(maximumRange: Double, tankCapacity: Double, var passengerCapacity: Int) : Aircraft3(maximumRange, tankCapacity)
+
+
+open class Aircraft3(var maximumRange: Double, var tankCapacity: Double) {
+    val fuelConsumption
+        get() = tankCapacity / maximumRange * 100
+}
